38e51f903b2f95bf671b09080f161116a4598cec
krati.chaturvedi@gmail.com <krati.chaturvedi@gmail.com>
2021-05-04 18:08:13 +0530

    tests updated

--- api/utils.py
+++ api/utils.py
@@ -1,8 +1,13 @@
+# File for utility functions
+
 from rest_framework.response import Response
 from rest_framework import status
 
 
 def error_prompt(error):
+    '''
+    Returns a Response object for error in api request
+    '''
     return Response(
         {'error': str(error)},
         status=status.HTTP_400_BAD_REQUEST
